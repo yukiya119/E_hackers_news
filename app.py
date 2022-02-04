0aa89b02248afe24a1e04e3b2c15b306313e2e52
yukiya119 <yukinosuke911@gmail.com>
2022-02-04 17:04:59 +0900

    リスト取得できることを確認
    30件で取得を止めたいと考え、いろいろトライしたけど実現できず
    コードがいまいち理解できていない

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ import requests
 def requests_top():
     url = 'https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty'  # New, Top and Best Stories
     response = requests.get(url)  # response = requests.get(URL, その他任意の引数)
-    list = response.json()
-    for i in list:
+    article = response.json()
+    for i in article:
         try:
             response2 = requests.get(f'https://hacker-news.firebaseio.com/v0/item/{i}.json?print=pretty').json()
             print('{ title:' + response2['title'], ', Link:' + response2['url'], '}')
